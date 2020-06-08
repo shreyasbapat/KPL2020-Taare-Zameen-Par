29c0ae0cecc3600f3a8bf02dfcb5fb215c1fabab
Shreyas Bapat <bapat.shreyas@gmail.com>
2020-06-08 21:16:20 +0530

    Solve first fifth

--- q5/main.py
+++ q5/main.py
@@ -14,15 +14,15 @@ as helper for this function.
 """
 
 class ScraperXRT:
-    
+
     '''
     Description
     -----------
     A class to scrap XRT files from the telescope archive.
     '''
-    
+
     def __init__(self, typeof_file, startime, endtime):
-        
+
         '''
         Parameters
         ----------
@@ -30,7 +30,7 @@ class ScraperXRT:
         startime: A `~datetime.datetime` instance
         endtime: A `~datetime.datetime` instance
         '''
-        
+
         self.urls = []
         self.downloads = []
         self.tof = typeof_file
@@ -38,13 +38,13 @@ class ScraperXRT:
         self.et = endtime
 
     def query(self):
-        
+
         '''
         Returns
         -------
         A `list` of strings of URLs.
-        '''    
-        
+        '''
+
         r = requests.get('http://solar.physics.montana.edu/HINODE/XRT/QL/syn_comp_fits/')
         soup = BeautifulSoup(r.content , 'html5lib')
         q = 'XRT_' + self.tof + '_' + self.st + '_' + self.et
@@ -55,13 +55,13 @@ class ScraperXRT:
         return self.urls
 
     def get(self):
-        
+
         '''
         Returns
         -------
         A `list` of strings for files.
         '''
-        
+
         for i in self.urls:
             r1 = requests.get(i)
             filename = i[61:]
@@ -71,12 +71,12 @@ class ScraperXRT:
         return self.downloads
 
     def view(self, filepath):
-        
+
         '''
         Parameters
         ----------
         filepath: A `string` representing absolute path of file in system.
-        
+
         Returns
         -------
         An instance of `matplotlib.image.AxesImage`, returned using `plt.imshow(data)`.
