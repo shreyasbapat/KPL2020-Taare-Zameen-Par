8317c4b25ec7d852aaa78d7fd5d2d7182724fd2d
Shreyas Bapat <bapat.shreyas@gmail.com>
2020-06-08 21:37:22 +0530

    Add q6

--- q6/main.py
+++ q6/main.py
@@ -4,6 +4,12 @@
 Any extra lines of code (if required)
 as helper for this function.
 """
+def findDist(v, theta):
+	g = 9.807 
+	h = 8848
+	t = math.sqrt((2*g*h) + v*v*math.sin(theta)*math.sin(theta))
+	return t*v*math.cos(theta)/g
+
 
 def findstrike(velocity, alt, az):
 	'''
@@ -12,9 +18,20 @@ def findstrike(velocity, alt, az):
 	velocity : A `float`
     alt: A `float`
     az: A `float`
-	
+
 	Returns
 	-------
 	A `tuple` of two floats
 	'''
-	return NotImplementedError
+
+	v = velocity
+	lat1 = 27.9881
+	lon1 = 86.9250
+	earth_radius = 6.3781e6
+	distance = findDist(v, math.radians(alt))
+	b = distance/earth_radius
+	a = math.acos(math.cos(b)*math.cos(90-lat1) + math.sin(90-lat1)*math.sin(b)*math.cos(az))
+	B = math.asin(math.sin(b)*math.sin(az)/math.sin(a))
+	lat2 = 90 - a
+	lon2 = B + lon1
+	return [lat2, lon2]
