45dd1541880d3bae32284010895a2442c4244c60
Shreyas Bapat <bapat.shreyas@gmail.com>
2020-06-08 18:58:56 +0530

    Solve third

--- q3/main.py
+++ q3/main.py
@@ -1,5 +1,6 @@
 # all imports below
-
+from astropy import constants
+from astropy import units as u
 """
 Any extra lines of code (if required)
 as helper for this function.
@@ -10,9 +11,18 @@ def findDelay(dist):
 	Parameters
 	----------
 	dist : A `float`
-	
+
 	Returns
 	-------
 	A `float`
 	'''
-	return NotImplementedError
+	dist = dist * u.m
+	Me = 5.94 * 10**24 * u.kg
+	c = constants.c
+	G = constants.G
+	Re = 6357000 * u.m
+	velocity = ((G * Me) / dist)**0.5
+	beta = velocity**2 / c**2
+	gamma = 1/ ((1 - beta)**0.5)
+	T = 1 * gamma
+	return T.value
